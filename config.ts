97bc70440dadcecd2c67e6e802dea45651cd2535
Yaotzin Sanabria <yaosanabria@gmail.com>
2024-08-02 18:43:11 -0600

    fix: resolve window is not defined issue in BASE_URL

--- config.ts
+++ config.ts
@@ -1,3 +1,5 @@
 export const NEXTAUTH_SECRET = process.env.NEXTAUTH_SECRET;
 export const SALT_HASH_ROUNDS = 12;
-export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || window.origin;
+export const BASE_URL =
+	process.env.NEXT_PUBLIC_BASE_URL ||
+	(typeof window !== 'undefined' ? window.origin : undefined);
